1e352183c8c47ac4df15f07ad9210c4cbf539c27
Mateja <mateja@mateja.local>
2019-11-14 16:30:40 +0100

    Add images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-//const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 // //const concat = require('gulp-cancat');
