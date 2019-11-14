cd6f83466aa153670fb1e3c17f7c71495003f8b4
Mateja <mateja@mateja.local>
2019-11-14 16:16:04 +0100

    Final commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,89 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
-//const concat = require('gulp-cancat');
+// //const concat = require('gulp-cancat');
 
+gulp.task('message', done => {
+    return console.log('Gulp is running...');
+    done();
+});
 
 //Message
-gulp.task('message', async function(){
-  return console.log('Gulp is running...');
-});
+// gulp.task('message', function(){
+//   return console.log('Gulp is running...');
+// });
 
-//Copy All HTML
-gulp.task('copyHtml', async function(){
-  gulp.src('src/*.html')
+gulp.task('copyHtml', done => {
+    gulp.src('src/*.html')
       .pipe(gulp.dest('dist'));
+    done();
 });
 
-//Optimize images
-gulp.task('imageMin', async () =>
-	gulp.src('src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
-);
+// //Copy All HTML
+// gulp.task('copyHtml', function(){
+//   gulp.src('src/*.html')
+//       .pipe(gulp.dest('dist'));
+// });
 
-//Optimize js
-gulp.task('minify', async function(){
-  gulp.src('src/js/*.js')
+// gulp.task('imageMin', done => {
+//     gulp.src('src/images/*')
+//       .pipe(imagemin())
+//       .pipe(gulp.dest('dist/images'))
+//     done();
+// });
+
+// //Optimize images
+// gulp.task('imageMin', () =>
+// 	gulp.src('src/images/*')
+// 		.pipe(imagemin())
+// 		.pipe(gulp.dest('dist/images'))
+// );
+
+gulp.task('minify', done => {
+    gulp.src('src/js/*.js')
       .pipe(uglify())
       .pipe(gulp.dest('dist/js'));
+    done();
+});
+
+// //Optimize js
+// gulp.task('minify', function(){
+//   gulp.src('src/js/*.js')
+//       .pipe(uglify())
+//       .pipe(gulp.dest('dist/js'));
+// });
+
+gulp.task('sass', done => {
+    gulp.src('src/sass/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('dist/css'));
+    done();
 });
 
 //Compile Sass
-gulp.task('sass', async function(){
-  gulp.src('src/sass/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('dist/css'));
-});
+// gulp.task('sass', function(){
+//   gulp.src('src/sass/*.scss')
+//       .pipe(sass().on('error', sass.logError))
+//       .pipe(gulp.dest('dist/css'));
+// });
 
-//Scripts (if more js files + dodati file u src, iz default izbrisati minify
-//gulp.task('scripts', async function(){
-  //gulp.src('src/js/*.js')
-      //.pipe(concat('main.js'))
-      //.pipe(uglify())
-      //.pipe(gulp.dest('dist/js'));
-//});
+// Scripts (if more js files + dodati file u src, iz default izbrisati minify
+// gulp.task('scripts', async function(){
+//   gulp.src('src/js/*.js')
+//       .pipe(concat('main.js'))
+//       .pipe(uglify())
+//       .pipe(gulp.dest('dist/js'));
+// });
 
-//gulp.task('default', ['message', 'copyHtml', 'imageMin', 'minify', 'sass']);
+// gulp.task('default', ['message', 'copyHtml', 'imageMin', 'minify', 'sass']);
 
-//gulp.task('watch, async function(){
-    //gulp.watch('src/js/*.js', ['scripts']);
-    //gulp.watch('src/images/*', ['imageMin']);
-    //gulp.watch('src/sass/*.scss', ['sass']);
-    //gulp.watch('src/*.html', ['copyHtml']);
-//}');
+gulp.task('default', gulp.series('copyHtml', 'minify', 'sass'));
+
+// gulp.task('watch, async function(){
+//     gulp.watch('src/js/*.js', ['scripts']);
+//     gulp.watch('src/images/*', ['imageMin']);
+//     gulp.watch('src/sass/*.scss', ['sass']);
+//     gulp.watch('src/*.html', ['copyHtml']);
+// }');
+
--- src/js/file.js
+++ src/js/file.js
@@ -1,2 +1,10 @@
 console.log('This is file 1');
 console.log('This is flie 2');
+
+document.getElementById('burger').addEventListener("click", openMenu);
+
+function openMenu(){
+    console.log('kliknut sam');
+    document.getElementById('dropdown').classList.toggle("active");
+    document.getElementById('burger').classList.toggle("active");
+}
